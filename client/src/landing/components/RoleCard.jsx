cda09c96b97d51a8c5083353c3dbd58841a91c7b
Neel Desai <neeldesai1085@gmail.com>
2025-10-02 20:58:49 +0530

    Corrected the routes of different roles

--- client/src/landing/components/RoleCard.jsx
+++ client/src/landing/components/RoleCard.jsx
@@ -14,7 +14,19 @@ export default function RoleCard({
   icon: Icon,
   roleParam,
 }) {
-  const isNGO = roleParam === "ngo";
+  // Decide routes based on role
+  let loginPath = "/";
+  let signupPath = null;
+
+  if (roleParam === "ngo") {
+    loginPath = "/proponent";
+    signupPath = "/proponent/signup";
+  } else if (roleParam === "admin") {
+    loginPath = "/admin";
+  } else if (roleParam === "auditor") {
+    loginPath = "/auditor";
+  }
+
   return (
     <Card className="hover-elevate transition-all duration-200 h-full">
       <CardHeader className="text-center pb-4">
@@ -30,7 +42,7 @@ export default function RoleCard({
       <CardContent className="pt-0">
         <div className="flex flex-col gap-3">
           {/* Login button */}
-          <Link to="/dummy">
+          <Link to={loginPath}>
             <Button
               variant="default"
               className="w-full cursor-pointer"
@@ -40,9 +52,9 @@ export default function RoleCard({
             </Button>
           </Link>
 
-          {/* Signup button - only for NGOs */}
-          {isNGO ? (
-            <Link to="/dummy">
+          {/* Signup button only for NGO */}
+          {signupPath ? (
+            <Link to={signupPath}>
               <Button
                 variant="outline"
                 className="w-full cursor-pointer"
