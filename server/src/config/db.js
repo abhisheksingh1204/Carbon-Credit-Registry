451e4cde184ed41996069367f643502d0a5e904f
Neel Desai <neeldesai1085@gmail.com>
2025-10-05 17:57:06 +0530

    Removed duplicate app.js and standardized environment variable name

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MYURL, {
+    await mongoose.connect(process.env.MY_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
