63c97388696124c8ec0f487f65869d398090c5f0
Neel Desai <neeldesai1085@gmail.com>
2025-10-06 03:55:18 +0530

    Corrected Proponent.jsx to render notfound page

--- client/src/proponent/Proponent.jsx
+++ client/src/proponent/Proponent.jsx
@@ -1,20 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "../components/Footer";
 import Login from "./pages/auth/Login";
 import NotFound from "../pages/NotFound";
 
-function ProponentRoutes() {
-  return (
-    <Routes>
-      <Route index element={<Login />} />
-      <Route path="auth/login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
-
-function Proponent() {
+function ProponentLayout() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <a href="#main-content" className="skip-to-content">
@@ -22,11 +12,21 @@ function Proponent() {
       </a>
       <Header />
       <main id="main-content" className="flex-1">
-        <ProponentRoutes   />
+        <Outlet />
       </main>
       <Footer />
     </div>
   );
 }
 
-export default Proponent;
+export default function Proponent() {
+  return (
+    <Routes>
+      <Route element={<ProponentLayout />}>
+        <Route index element={<Login />} />
+        <Route path="auth/login" element={<Login />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
